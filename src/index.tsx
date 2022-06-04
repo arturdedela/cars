fbdc5636701076956fd28d13fc4ec3d6bc681947
archid <archid@yandex-team.ru>
2022-06-04 13:12:24 +0530

    Add prettier

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
   <React.StrictMode>
-      <div>App</div>
-  </React.StrictMode>
+    <div>App</div>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
