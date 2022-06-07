625450b737217e13d0e81091aead4a0c61516e8c
archid <archid@yandex-team.ru>
2022-06-08 00:19:08 +0530

    Add NotFoundPage styles

--- src/app/NotFoundPage/NotFoundPage.tsx
+++ src/app/NotFoundPage/NotFoundPage.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { styled, Typography } from '@mui/material';
+import Logo from '../../assets/logo.png';
+import { Image, Link } from '../../ui';
 
 export interface NotFoundPageProps {}
 
+const Root = styled('div')(({ theme }) => ({
+  flex: 1,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  rowGap: theme.spacing(3),
+}));
+
 const NotFoundPage: React.FC<NotFoundPageProps> = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <Root>
+      <Image src={Logo} alt="Logo" width={175} />
+      <Typography variant="h1">404 - Not Found</Typography>
+      <Typography variant="body1">Sorry, the page you are looking for does not exist.</Typography>
+      <Typography variant="body1">
+        You can always go back to the <Link to="/cars">homepage</Link>.
+      </Typography>
+    </Root>
+  );
 };
 
 export default NotFoundPage;
